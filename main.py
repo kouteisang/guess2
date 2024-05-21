3c93b1f56b705ce9fb934f4c07281ceaf18a948b
Cheng Huang <80028318+kouteisang@users.noreply.github.com>
2024-05-21 10:49:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -20,15 +20,3 @@ if __name__ == '__main__':
         for m in ms:
             get_res("dbpedia", k, m, type)
             get_res("lmdb", k, m, type)
-
-    ## Test KMeans
-    # get_res_k_means("dbpedia", "distmult")
-    # get_res_k_means("lmdb", "distmult")
-
-
-# java -jar esummeval_v1.2.jar /Users/huangcheng/Documents/ESBasedonSimilarity/ESBM_benchmark_v1.2 /Users/huangcheng/Documents/ESBasedonSimilarity/res_data/esbm/k_3_m_2
-
-# java -jar esummeval_v1.2.jar /Users/huangcheng/Documents/ESBasedonSimilarity/ESBM_benchmark_v1.2 /Users/huangcheng/Documents/ESBasedonSimilarity/res_data/esbm_plus/complete_k_3_m_2
-
-# java -jar esummeval_v1.2.jar /Users/huangcheng/Documents/ESBasedonSimilarity/ESBM_benchmark_v1.2 /Users/huangcheng/Documents/ESBasedonSimilarity/res_data/KMeansClustering
-# java -jar esummeval_v1.2.jar /Users/huangcheng/Documents/ESBasedonSimilarity/ESBM_benchmark_v1.2 /Users/huangcheng/Documents/ESBasedonSimilarity/res_data/esbm_plus_test/complete_k_3_m_2
